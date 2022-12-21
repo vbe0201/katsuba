b5027a1335bef2139b380e1f33499b2e44065213
Valentin B <valentin.be@protonmail.com>
2022-12-21 17:49:25 +0100

    py: Forgot to register submodules

--- py/src/lib.rs
+++ py/src/lib.rs
@@ -17,18 +17,22 @@ fn kobold_py(py: Python, m: &PyModule) -> PyResult<()> {
     // Initialize the kobold.nav submodule.
     let nav = PyModule::new(py, "nav")?;
     nav::kobold_nav(nav)?;
+    m.add_submodule(nav)?;
 
     // Initialize the kobold.op submodule.
     let op = PyModule::new(py, "op")?;
     object_property::kobold_op(op)?;
+    m.add_submodule(op)?;
 
     // Initialize the kobold.poi submodule.
     let poi = PyModule::new(py, "poi")?;
     poi::kobold_poi(poi)?;
+    m.add_submodule(poi)?;
 
     // Initialize the kobold.wad submodule.
     let wad = PyModule::new(py, "wad")?;
     wad::kobold_wad(wad)?;
+    m.add_submodule(wad)?;
 
     Ok(())
 }
