1c41d5210469e355bbac944cb8ea9bab18a1ee7e
Valentin B <valentin.be@protonmail.com>
2023-06-23 13:48:59 +0200

    cli: Pre-allocate archive directories for Windows speedup

--- src/kobold/src/wad.rs
+++ src/kobold/src/wad.rs
@@ -31,15 +31,19 @@ pub fn process(wad: Wad) -> anyhow::Result<()> {
             out,
             verify_checksums,
         } => {
-            let out = match out {
+            let mut out = match out {
                 Some(out) => out,
                 None => std::env::current_dir()?,
             };
 
-            extract::extract_all(
-                Archive::mmap(&input, verify_checksums)?,
-                out.join(input.file_stem().unwrap()),
-            )
+            // TODO: Choose between heap and mmap based on file size.
+            let archive = Archive::mmap(&input, verify_checksums)?;
+
+            // At this point, we succeeded in opening `input` as a file.
+            // Therefore, we know it has a parent path that is not None.
+            out.push(input.file_stem().unwrap());
+
+            extract::extract_all(archive, out)
         }
     }
 }
--- src/kobold/src/wad/extract.rs
+++ src/kobold/src/wad/extract.rs
@@ -1,29 +1,39 @@
-use std::{fs, path::PathBuf};
+use std::{
+    collections::HashSet,
+    fs,
+    path::{Path, PathBuf},
+};
 
 use kobold_wad::{Archive, Inflater};
 
 pub fn extract_all(archive: Archive, out: PathBuf) -> anyhow::Result<()> {
     let mut inflater = Inflater::new();
 
+    // Pre-compute the directory structure we need to create.
+    let mut out_paths = HashSet::new();
+    for (file, _) in archive.files() {
+        let file: &Path = file.as_ref();
+        if let Some(p) = file.parent() {
+            out_paths.insert(p);
+        }
+    }
+
+    // Now create all the directories.
+    for path in out_paths {
+        fs::create_dir_all(out.join(path))?;
+    }
+
+    // Extract all the files in the archive.
     for (path, file) in archive.files() {
         // Get the uncompressed contents of the file.
         let contents = archive.file_contents(file);
-        let contents = if file.compressed {
-            inflater.decompress(contents, file.uncompressed_size as _)?
-        } else {
-            contents
+        let contents = match file.compressed {
+            true => inflater.decompress(contents, file.uncompressed_size as _)?,
+            false => contents,
         };
 
-        // Make sure the parent directory for the output file exists.
-        let out = out.join(path);
-        if let Some(p) = out.parent() {
-            if !p.exists() {
-                fs::create_dir_all(p)?;
-            }
-        }
-
         // Write uncompressed file contents.
-        fs::write(out, contents)?;
+        fs::write(out.join(path), contents)?;
     }
 
     Ok(())
