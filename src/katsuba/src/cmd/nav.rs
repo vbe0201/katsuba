48becdcc2044f813c5e1e0c80ca5e1212f596c7f
Marcus S <booleanbirch@users.noreply.github.com>
2025-11-11 00:29:51 -0600

    Implemented C API for nav commands

--- src/katsuba/src/cmd/nav.rs
+++ src/katsuba/src/cmd/nav.rs
@@ -1,8 +1,15 @@
+use std::{
+    ffi::{CStr},
+    path::PathBuf,
+};
+
+use libc::{c_char};
+
 use clap::{Args, Subcommand, ValueEnum};
 use katsuba_nav::{NavigationGraph, ZoneNavigationGraph};
 
 use super::Command;
-use crate::cli::{helpers, Bias, InputsOutputs, Processor};
+use crate::cli::{helpers, Bias, InputsOutputs, Processor, HYPHEN};
 
 /// Subcommand for working with NAV data.
 #[derive(Debug, Args)]
@@ -57,4 +64,70 @@ fn deserialize_zonenav(args: InputsOutputs) -> eyre::Result<()> {
         .read_with(|r, _| ZoneNavigationGraph::parse(r).map_err(Into::into))
         .write_with(helpers::write_as_json)
         .process(inputs, outputs)
+}
+
+#[no_mangle]
+pub extern "C" fn nav_deserialize(
+    input: *const c_char,
+    output: *const c_char,
+) -> bool {
+    let default_path = PathBuf::from(HYPHEN);
+
+    let rust_input = if input.is_null() {
+        return false
+    } else {
+        match unsafe { CStr::from_ptr(input) }.to_str() {
+            Ok(rust_str) => rust_str.to_owned(),
+            Err(_) => return false,
+        }
+    };
+
+    let rust_output = if output.is_null() {
+        default_path
+    } else {
+        match unsafe { CStr::from_ptr(output) }.to_str() {
+            Ok(rust_str) => PathBuf::from(rust_str),
+            Err(_) => default_path,
+        }
+    };
+
+    let io = InputsOutputs {
+        input: rust_input,
+        output: rust_output,
+    };
+
+    deserialize_nav(io).is_ok()
+}
+
+#[no_mangle]
+pub extern "C" fn zonenav_deserialize(
+    input: *const c_char,
+    output: *const c_char,
+) -> bool {
+    let default_path = PathBuf::from(HYPHEN);
+
+    let rust_input = if input.is_null() {
+        return false
+    } else {
+        match unsafe { CStr::from_ptr(input) }.to_str() {
+            Ok(rust_str) => rust_str.to_owned(),
+            Err(_) => return false,
+        }
+    };
+
+    let rust_output = if output.is_null() {
+        default_path
+    } else {
+        match unsafe { CStr::from_ptr(output) }.to_str() {
+            Ok(rust_str) => PathBuf::from(rust_str),
+            Err(_) => default_path,
+        }
+    };
+
+    let io = InputsOutputs {
+        input: rust_input,
+        output: rust_output,
+    };
+
+    deserialize_zonenav(io).is_ok()
 }
