a915c78b5be614edfc0849c14976a7eca3b118e4
Valentin B. <valentin.be@protonmail.com>
2026-02-14 12:52:34 +0100

    wad: Remove crc32fast dep in favor of libdeflater

--- src/katsuba-wad/src/crc.rs
+++ src/katsuba-wad/src/crc.rs
@@ -1,11 +1,11 @@
 //! CRC32 calculation for integrity-checking uncompressed
 //! archive files.
 
-use crc32fast::Hasher;
+use libdeflater::Crc;
 
 /// Computes the CRC32 of `data`, as encoded in KIWAD archives.
 pub fn hash(data: &[u8]) -> u32 {
-    let mut hasher = Hasher::new_with_initial(u32::MAX);
+    let mut hasher = Crc::with_initial(u32::MAX);
     hasher.update(data);
-    hasher.finalize() ^ u32::MAX
+    hasher.sum() ^ u32::MAX
 }
